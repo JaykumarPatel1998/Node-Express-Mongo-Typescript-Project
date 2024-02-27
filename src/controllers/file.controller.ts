b2cf9ab82d4abc525361ffc5c51e4de8db418989
jaykumar <slapbot6@gmail.com>
2024-02-27 13:23:20 -0500

    added code for semantic search using vector embeddings

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -4,6 +4,8 @@ import createPresignedUrlWithClient from "../fileUtils/presignedUrl";
 import deleteFile from "../fileUtils/delete";
 import UserModel from "../models/user";
 import createHttpError from "http-errors";
+import { generateEmbeddingFromS3Doc } from "../fileUtils/generateToken";
+import FileEmbeddings from "../models/fileEmbeddings";
 
 interface FileObject extends Express.Multer.File {
     bucket?: string,
@@ -30,7 +32,7 @@ export const getFiles: RequestHandler = async (req: ExtendedRequest, res, next) 
         }
         const user = await UserModel.findById(req.id);
         const messagesNotRead = user?.messageArray.filter(message => message.read === false)
-        return res.status(200).render(`index`, { userId: user?._id, files, messages: messagesNotRead});
+        return res.status(200).render(`index`, { userId: user?._id, files, messages: messagesNotRead });
     } catch (error) {
         next(error)
     }
@@ -45,7 +47,7 @@ export const uploadFile: RequestHandler = async (req: ExtendedRequest, res, next
         const seconds = date.getSeconds()
         date.setSeconds(seconds + Number.parseInt(process.env.EXPIRY_PRESIGNED_URL_USER!))
 
-        await new File({
+        const file = new File({
             bucket: options.bucket,
             fileName: options.originalname,
             key: options.key,
@@ -53,10 +55,72 @@ export const uploadFile: RequestHandler = async (req: ExtendedRequest, res, next
             fileUrl: presignedUrl,
             userId: req.id,
             urlExpiryDate: date
-        }).save()
+        })
 
-        res.status(200).redirect("/api/files/")
-        return;
+        await file.save()
+
+
+
+        const filenameArray = file.fileName.split('.')
+        if (filenameArray[filenameArray.length - 1] === "pdf") {
+            const htmlWaitState = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <style>
+            .loader {
+            border: 16px solid #f3f3f3; /* Light grey */
+            border-top: 16px solid #3498db; /* Blue */
+            border-radius: 50%;
+            width: 120px;
+            height: 120px;
+            animation: spin 2s linear infinite;
+            }
+            
+            @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+            }
+            </style>
+            </head>
+            <body>
+            
+                <div id="spinner">
+                    <h2>File saved succesfully, now generating vector embeddings. Thanks for being patient.</h2>
+                    <div class="loader"></div>
+                </div>
+
+                <div id="successful" style="display: none;">
+                    <h2>File embedded successfully</h2>
+                    <a href="/api/files">Se all files</a>
+                </div>
+            
+            </body>
+            </html>
+        `
+
+            res.write(htmlWaitState)
+            const embedding = await generateEmbeddingFromS3Doc(presignedUrl);
+
+            await new FileEmbeddings({
+                embedding: embedding,
+                fileId: file._id,
+                userId: req.id?.toString()
+            }).save()
+
+            res.status(200).end(`
+                <script>
+                    // Replace this with your actual logic
+                    document.getElementById('spinner').style.display = 'none';
+                    document.getElementById('successful').style.display = 'block';
+                </script>
+            `)
+            return;
+        } else {
+            return res.status(200).redirect('/api/files')
+        }
+
+
     } catch (error) {
         next(error)
     }
